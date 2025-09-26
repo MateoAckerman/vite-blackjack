578d979fa31cb4b923a4b85bbf4f1deaaba76b31
Mateo Lara <mateodev99@gmail.com>
2025-09-26 18:51:07 +0200

    Scoring - Bug Fixes

--- src/blackjack/index.js
+++ src/blackjack/index.js
@@ -73,7 +73,7 @@ btnDetener.addEventListener('click', () => {
     btnPedir.disabled   = true;
     btnDetener.disabled = true;
 
-    turnoComputadora( puntosJugador, puntosHTML, divCartasComputadora, deck );
+    turnoComputadora( puntosJugador, puntosHTML[1], divCartasComputadora, deck );
 });
 
 btnNuevo.addEventListener('click', () => {
